3a96e596ecdc198122962866552eaf858984157e
Sergey Pestov <pestov-sa@ya.ru>
2024-05-27 18:17:35 +0300

    layer 170 from official json

---
+++ examples/help-getConfig/index.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const MTProto = require("../../envs/node/index.js");
+
+const api_id = process.env.TELEGRAM_API_ID;
+const api_hash = process.env.TELEGRAM_API_HASH;
+
+// 1. Create instance
+const mtproto = new MTProto({
+  api_id,
+  api_hash,
+
+  storageOptions: {
+    path: path.resolve(__dirname, "../../data/1.json"),
+  },
+});
+
+// 2. Print the user country code
+mtproto.call("help.getConfig").then((result) => {
+  console.log(result);
+}).catch(err => {
+  console.error(err);
+});
